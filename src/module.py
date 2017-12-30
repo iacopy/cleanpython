2a4d1b4cc9eb1a248ffb2d6b4c49ee05fb331a21
iacopy <iacopomarmorini@gmail.com>
2017-12-30 15:55:36 +0100

    add sample module with test

---
+++ src/module.py
@@ -0,0 +1,20 @@
+"""
+Module example to show doctests and documentation results.
+"""
+
+
+def add(num_1, num_2):
+    """
+    Add 2 numbers
+    """
+    return num_1 + num_2
+
+
+def subtract(num_1, num_2):
+    """
+    Subtract two numbers.
+
+    >>> subtract(1, 3)
+    -2
+    """
+    return num_1 - num_2
---
+++ src/test_module.py
@@ -0,0 +1,20 @@
+"""
+Test stuff
+"""
+
+
+import pytest
+
+from module import add
+
+
+@pytest.mark.parametrize('num_1,num_2,expected', [
+    [1, 1, 2],
+    [1, 2, 3],
+    [-1, 2, 1],
+])
+def test_add(num_1, num_2, expected):
+    """
+    Just test add function in different cases
+    """
+    assert add(num_1, num_2) == expected
