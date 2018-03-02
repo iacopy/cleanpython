0d060143b2d7447af93c7580902edfc902892431
iacopy <iacopomarmorini@gmail.com>
2018-03-02 08:00:17 +0100

    code: refactor similar blocks of code (for codeclimate)

--- src/puzzle/puzzle.py
+++ src/puzzle/puzzle.py
@@ -11,13 +11,18 @@ def swap(ary, size, cell_0, cell_1):
     Given an array `ary`, swap `cell_0` with `cell_1`.
     Each cell is `size` x `size`.
     """
-    row_0 = range(cell_0[0] * size, cell_0[0] * size + size)
-    col_0 = range(cell_0[1] * size, cell_0[1] * size + size)
-    row_1 = range(cell_1[0] * size, cell_1[0] * size + size)
-    col_1 = range(cell_1[1] * size, cell_1[1] * size + size)
+    def iter_row(cell):
+        """Return the `cell` row iterator (implicit cell size).
+        """
+        return range(cell[0] * size, cell[0] * size + size)
 
-    for y_0, y_1 in zip(row_0, row_1):
-        for (x_0, x_1) in zip(col_0, col_1):
+    def iter_col(cell):
+        """Return the `cell` column iterator (implicit cell size).
+        """
+        return range(cell[1] * size, cell[1] * size + size)
+
+    for y_0, y_1 in zip(iter_row(cell_0), iter_row(cell_1)):
+        for (x_0, x_1) in zip(iter_col(cell_0), iter_col(cell_1)):
             # could not managed to do it without tmp
             tmp = copy(ary[y_0][x_0])
             ary[y_0][x_0] = ary[y_1][x_1]
