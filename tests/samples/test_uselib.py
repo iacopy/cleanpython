ae3956f95c4f02aa269fc738a92ec83e10c75f88
iacopo <iacopomarmorini@gmail.com>
2018-12-10 21:55:45 +0100

    add failing test with xfail

--- tests/samples/test_uselib.py
+++ tests/samples/test_uselib.py
@@ -1,6 +1,9 @@
 """
 Uselib module tests.
 """
+# 3rd party
+import pytest
+
 # My stuff
 from samples import uselib
 
@@ -8,3 +11,10 @@ from samples import uselib
 def test_uselib():
     """test"""
     assert uselib.use(1, 2) == 3
+
+
+@pytest.mark.xfail
+def test_fail():
+    import notexist
+    for i in range(notexist.nothing):
+        assert i == 0
