4c01fc963794edadf5434d6c0cb0387af394706b
iacopo <iacopomarmorini@gmail.com>
2022-10-20 16:58:48 +0200

    Format code with black
    Issue #33

--- confix.py
+++ confix.py
@@ -19,15 +19,15 @@ def fix_docs_conf_py():
         import sys
         sys.path.insert(0, os.path.abspath('../src'))
     """
-    with open('docs/conf.py', 'r', encoding='utf-8') as file_obj:
+    with open("docs/conf.py", "r", encoding="utf-8") as file_obj:
         lines = file_obj.readlines()
-    with open('docs/conf.py', 'w', encoding='utf-8') as file_obj:
+    with open("docs/conf.py", "w", encoding="utf-8") as file_obj:
         for line in lines:
-            if line.startswith('# import'):
+            if line.startswith("# import"):
                 # uncomment the line
                 line = line[2:]
-            if line.startswith('# sys.path.insert'):
-                line = '\nsys.path.insert(0, os.path.abspath(\'../src\'))\n'
+            if line.startswith("# sys.path.insert"):
+                line = "\nsys.path.insert(0, os.path.abspath('../src'))\n"
             file_obj.write(line)
 
 
@@ -42,22 +42,22 @@ def fix_docs_index_rst():
        modules
     """
     towrite = []
-    with open('docs/index.rst', 'r', encoding='utf-8') as file_obj:
+    with open("docs/index.rst", "r", encoding="utf-8") as file_obj:
         lines = file_obj.readlines()
     for line in lines:
         # Add "modules" to the toctree
         # raise an error if the string "modules" is already in the file
-        if line.startswith('   :caption: Contents:'):
-            towrite.extend(('   :caption: Contents:\n\n', '   modules\n'))
-        elif line.strip() == 'modules':
+        if line.startswith("   :caption: Contents:"):
+            towrite.extend(("   :caption: Contents:\n\n", "   modules\n"))
+        elif line.strip() == "modules":
             raise Exception('"modules" is already in docs/index.rst')
         else:
             towrite.append(line)
 
-    with open('docs/index.rst', 'w', encoding='utf-8') as file_obj:
-        file_obj.write(''.join(towrite))
+    with open("docs/index.rst", "w", encoding="utf-8") as file_obj:
+        file_obj.write("".join(towrite))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fix_docs_conf_py()
     fix_docs_index_rst()
