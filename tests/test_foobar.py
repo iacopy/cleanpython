f621e1540edda9a8e0830f23a06e52a3e25b7386
iacopo <iacopomarmorini@gmail.com>
2022-11-19 15:11:46 +0100

    test_foobar: add a basic integration test
    NB: TDD (test-driven-development: the test before the code)

--- tests/test_foobar.py
+++ tests/test_foobar.py
@@ -1,12 +1,24 @@
 """
 Basic test.
 """
+# Standard Library
+import os
+
+# 3rd party
+import pytest
+
 # My stuff
 import foobar
 
 
-def test_main():
+@pytest.mark.integration
+@pytest.mark.webtest
+def test_main(tmp_path):
     """
     Test main function.
     """
-    foobar.main()
+    res = foobar.main(["https://en.wikipedia.org/wiki/Wikipedia"], tmp_path)
+    assert len(res) == 1
+    title, dest = res[0]
+    assert title == "Wikipedia - Wikipedia"
+    assert os.path.exists(dest)
